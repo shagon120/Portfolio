844e23f978ba4530420f476db3b4367812b969ef
suhayb <suhaybaljumaili@gmail.com>
2024-03-06 00:55:23 +0000

    first commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React from "react";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const About = () => {
-  return <div>About</div>;
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+  return (
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div
+          className="flex flex-col gap-y-10 lg:flex-row lg:items-center *:lg:gap-x-20 
+        lg:gap-y-0 h-screen"
+        >
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[740px]
+          mix-blind-lighten bg-top"
+          ></motion.div>
+
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent">About me.</h2>
+            <h3 className="h3 mb-4">
+              I'm a Front-end Developer With 2 Years of experience.
+            </h3>
+            <p className="mb-6">
+              I'm a passionate person with vast background experience In Next.js
+              with a bit of Css combining it with Html as well as Digital
+              marketing for ecommerce and local businesses. looking to develop
+              myself into a rewarding Career in web development.
+            </p>
+            <div className="flex">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={20} end={2} duration={5} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px] mb-3">
+                  {" "}
+                  Years of <br /> Experience
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-x-8 items-center">
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
-import React from 'react';
-
+import React from "react";
+import Logo from "../assets/logo1.png";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className=" py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center mb-5">
+          <a href="#">
+            <img src={Logo} height={235} width={235} alt="" />
+          </a>
+          <button className="btn btn-sm ">Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
